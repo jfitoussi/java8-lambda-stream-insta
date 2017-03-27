1d969bcaa43ac522b7b1122f8ee07e9e1de13508
phayusa <sokomo873@free.fr>
2017-03-27 15:48:36 +0200

    Exercice change.

--- stream-api/src/test/java/stream/api/Exercise4Test.java
+++ stream-api/src/test/java/stream/api/Exercise4Test.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.is;
@@ -23,7 +24,7 @@ public class Exercise4Test extends ClassicOnlineStore {
          * Find the first customer who registered this online store by using {@link Stream#findFirst}
          * The customerList are ascending ordered by registered timing.
          */
-        Optional<Customer> firstCustomer = null;
+        Optional<Customer> firstCustomer = customerList.stream().findFirst();
 
         assertThat(firstCustomer.get(), is(customerList.get(0)));
     }
@@ -35,7 +36,9 @@ public class Exercise4Test extends ClassicOnlineStore {
         /**
          * Check whether any customer older than 40 exists or not, by using {@link Stream#anyMatch}
          */
-        boolean olderThan40Exists = true;
+        Predicate<Customer> prediOlder = customer -> customer.getAge() > 40;
+
+        boolean olderThan40Exists = customerList.stream().anyMatch(prediOlder);
 
         assertThat(olderThan40Exists, is(false));
     }
@@ -47,7 +50,8 @@ public class Exercise4Test extends ClassicOnlineStore {
         /**
          * Check whether all customer are older than 20 or not, by using {@link Stream#allMatch}
          */
-        boolean allOlderThan20 = false;
+        Predicate<Customer> predicate = customer -> customer.getAge() > 20;
+        boolean allOlderThan20 = customerList.stream().allMatch(predicate);
 
         assertThat(allOlderThan20, is(true));
     }
@@ -60,7 +64,8 @@ public class Exercise4Test extends ClassicOnlineStore {
          * Confirm that none of the customer has empty list for their {@link Customer.wantToBuy}
          * by using {@link Stream#noneMatch}
          */
-        boolean everyoneWantsSomething = false;
+        Predicate<Customer> predicate = customer -> !customer.getWantToBuy().isEmpty();
+        boolean everyoneWantsSomething = customerList.stream().anyMatch(predicate);
 
         assertThat(everyoneWantsSomething, is(true));
     }
